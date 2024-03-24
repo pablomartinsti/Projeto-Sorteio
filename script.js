4e920973cbc4f86966367bebc079e3ecc27d80d0
pablomartinsti <pablomartinsti@outlook.com>
2024-03-24 12:47:50 -0300

    alteraçao

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function gerar() {
     
     const min = Math.ceil( document.querySelector(".começo").value)
     const max = Math.floor (document.querySelector(".fim").value)
+
+    if (min >= max){
+
+        alert("o valor minimo tem que ser menor que o maximo")
+
+    }
+
     var resp = document.querySelector('.resp');
     resp.innerHTML = Math.floor(Math.random() * (max - min + 1) ) + min
+
+    
+
+
+   
 }
